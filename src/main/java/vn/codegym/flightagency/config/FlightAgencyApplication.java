3b6767838a72aa116f277f7874324a923e8e2578
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-08-31 18:18:46 +0700

    init

---
+++ src/main/java/vn/codegym/flightagency/config/FlightAgencyApplication.java
@@ -0,0 +1,13 @@
+package vn.codegym.flightagency.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightAgencyApplication.class, args);
+    }
+
+}
