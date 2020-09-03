17faabd388c4ad2693706c2abe12c176db198f32
ADMIN <mauminhka19@gmail.com>
2020-09-03 15:34:51 +0700

    update customer Repo, Service

---
+++ src/main/java/vn/codegym/flightagency/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,8 @@
+package vn.codegym.flightagency.service.impl;
+
+import org.springframework.stereotype.Service;
+import vn.codegym.flightagency.service.CustomerService;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+}
