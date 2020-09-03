2e1d98b8c5a383588266d42e5624b1d8ba41a610
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-09-03 18:35:54 +0700

    Bảng Aircraft, Airport, Passenger

--- src/main/java/vn/codegym/flightagency/model/Aircraft.java
+++ src/main/java/vn/codegym/flightagency/model/Aircraft.java
@@ -1,13 +1,10 @@
 package vn.codegym.flightagency.model;
 
-
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "aircrafts")
--- src/main/java/vn/codegym/flightagency/model/Airport.java
+++ src/main/java/vn/codegym/flightagency/model/Airport.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "airport")
+@Table(name = "airports")
 @Getter
 @Setter
 @NoArgsConstructor
