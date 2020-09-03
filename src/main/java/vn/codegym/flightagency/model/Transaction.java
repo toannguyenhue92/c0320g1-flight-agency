16d75c1350a2ec615b1d77829f18e08725a8aa9f
NguHoaDuy <62531944+NguHoaDuy@users.noreply.github.com>
2020-09-03 15:24:29 +0700

    update flight schedule

---
+++ src/main/java/vn/codegym/flightagency/model/Transaction.java
@@ -0,0 +1,52 @@
+package vn.codegym.flightagency.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_schedule_id", nullable = false)
+    private FlightSchedule flightSchedule;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "transaction_passenger",
+            joinColumns = @JoinColumn(name = "transaction_id"),
+            inverseJoinColumns = @JoinColumn(name = "passenger_id")
+    )
+    @JsonManagedReference
+    private List<Passenger> passengers;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "created_time")
+    private LocalDateTime createdTime;
+
+    @Column(name = "due_time")
+    private LocalDateTime dueTime;
+
+    @Column(name = "status")
+    private String status;
+}
