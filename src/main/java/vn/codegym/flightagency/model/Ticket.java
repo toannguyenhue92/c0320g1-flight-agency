f2f17c9bcb7900b17e5e8ea187cd4d9db483c0d4
NguyenDucThong <Nguyenducthong22>
2020-09-09 14:01:02 +0700

    C-Thong them ticket controller, service, repository

--- src/main/java/vn/codegym/flightagency/model/Ticket.java
+++ src/main/java/vn/codegym/flightagency/model/Ticket.java
@@ -1,7 +1,7 @@
 package vn.codegym.flightagency.model;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "ticket")
@@ -18,10 +18,10 @@ public class Ticket {
     private String destination;
 
     @Column(name = "depature_time")
-    private Date depatureTtime;
+    private LocalDateTime depatureTime;
 
     @Column(name = "arrival_time")
-    private Date arrivalTime;
+    private LocalDateTime arrivalTime;
 
     @Column(name = "booking_code")
     private String bookingCode;
@@ -60,5 +60,142 @@ public class Ticket {
     private String extraLuggage;
 
 
+    public Ticket() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepature() {
+        return depature;
+    }
+
+    public void setDepature(String depature) {
+        this.depature = depature;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public LocalDateTime getDepatureTtime() {
+        return depatureTime;
+    }
+
+    public void setDepatureTtime(LocalDateTime depatureTtime) {
+        this.depatureTime = depatureTtime;
+    }
+
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalDateTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getBookingCode() {
+        return bookingCode;
+    }
+
+    public void setBookingCode(String bookingCode) {
+        this.bookingCode = bookingCode;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getTypeTicket() {
+        return typeTicket;
+    }
+
+    public void setTypeTicket(String typeTicket) {
+        this.typeTicket = typeTicket;
+    }
+
+    public String getChair() {
+        return chair;
+    }
+
+    public void setChair(String chair) {
+        this.chair = chair;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getTaxesAndFees() {
+        return taxesAndFees;
+    }
+
+    public void setTaxesAndFees(int taxesAndFees) {
+        this.taxesAndFees = taxesAndFees;
+    }
+
+    public String getTypeCustomer() {
+        return typeCustomer;
+    }
+
+    public void setTypeCustomer(String typeCustomer) {
+        this.typeCustomer = typeCustomer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getExtraLuggage() {
+        return extraLuggage;
+    }
+
+    public void setExtraLuggage(String extraLuggage) {
+        this.extraLuggage = extraLuggage;
+    }
 }
---
+++ src/main/java/vn/codegym/flightagency/repository/TicketRepository.java
@@ -0,0 +1,14 @@
+package vn.codegym.flightagency.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.codegym.flightagency.model.Ticket;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket,Long> {
+    Page<Ticket> findAllByNameContaining(String search, Pageable pageable);
+    Page<Ticket> findAllByBookingCodeContaining(String search, Pageable pageable);
+    Page<Ticket> findAllByDepatureContaining(String search, Pageable pageable);
+}
