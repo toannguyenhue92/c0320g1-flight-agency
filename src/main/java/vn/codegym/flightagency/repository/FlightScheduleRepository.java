bccdd477db713a72d19d51b6d99d7af65a0e52f4
CheCongBach <chebach97bk@gmail.com>
2020-09-09 11:33:01 +0700

    Hoàn thiện phần hiển thị thông tin chuyến bay

--- src/main/java/vn/codegym/flightagency/repository/FlightScheduleRepository.java
+++ src/main/java/vn/codegym/flightagency/repository/FlightScheduleRepository.java
@@ -1,10 +1,31 @@
 package vn.codegym.flightagency.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import vn.codegym.flightagency.model.FlightSchedule;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface FlightScheduleRepository extends JpaRepository<FlightSchedule, Long> {
 
+//    @Query(nativeQuery = true, value =
+//            "Select * from flight_schedules where " +
+//            "departure_airport_id = ?1 and " +
+//            "arrival_airport_id = ?2  and " +
+//            "status = 'active' and " +
+//            "departure_time between ?3 and ?4 "
+//    )
+    @Query(value =
+            "Select f from FlightSchedule f where " +
+            "f.departureAirport.id = ?1 and " +
+            "f.arrivalAirport.id = ?2  and " +
+            "f.status = 'active' and " +
+            "f.departureDateTime >= ?3 and " +
+            "f.departureDateTime <= ?4"
+    )
+    List<FlightSchedule> findAllFlightSchedules(Long depCode, Long arrCode, LocalDateTime from, LocalDateTime to, Sort sortBy);
 }
--- src/main/java/vn/codegym/flightagency/service/FlightScheduleService.java
+++ src/main/java/vn/codegym/flightagency/service/FlightScheduleService.java
@@ -3,8 +3,15 @@ package vn.codegym.flightagency.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import vn.codegym.flightagency.model.FlightSchedule;
+import vn.codegym.flightagency.dto.FlightSearchDTO;
+import vn.codegym.flightagency.model.FlightSchedule;
 
+import java.time.LocalDate;
+import java.util.List;
 public interface FlightScheduleService {
     //D-Bach
-    Page<FlightSchedule> findAllFlightSchedule (Pageable pageable);
+    Page<FlightSchedule> findAllFlightSchedule(Pageable pageable);
+
+    //A-Duy
+    List<FlightSchedule> searchFlights(FlightSearchDTO flights);
 }
