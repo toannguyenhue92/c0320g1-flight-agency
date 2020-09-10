75e994016e1234c6650c8af9a692508d8edcd80e
hothaodn <62532665+hothaodn@users.noreply.github.com>
2020-09-10 09:50:08 +0700

    B-Thao push code to review

--- src/main/java/vn/codegym/flightagency/repository/PromoRepository.java
+++ src/main/java/vn/codegym/flightagency/repository/PromoRepository.java
@@ -1,10 +1,47 @@
 package vn.codegym.flightagency.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import vn.codegym.flightagency.model.Promo;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface PromoRepository extends JpaRepository<Promo, Long> {
 
+    @Query(value = "select * from flight_agency.promo " +
+            "where ?1 between promo_date_start and promo_date_end",
+            nativeQuery = true)
+    List<Promo> getPromoRunningNow(LocalDateTime time, Pageable pageable);
+
+    @Query(value = "select * from flight_agency.promo " +
+            "where promo_date_end < ?1",
+            nativeQuery = true)
+    List<Promo> getPromoExpired(LocalDateTime time, Pageable pageable);
+
+    @Query(value = "select * from flight_agency.promo " +
+            "where promo_date_start > ?1",
+            nativeQuery = true)
+    List<Promo> getPromoNotActive(LocalDateTime time, Pageable pageable);
+
+    @Query(value = "select * from flight_agency.promo\n" +
+            "where name_promo like %?1%" +
+            "and airline like ?2" +
+            "and departure_place like ?3" +
+            "and arrival_place like ?4" +
+            "and ((?5 between promo_date_start and promo_date_end)" +
+            "or (?6 between promo_date_start and promo_date_end))" +
+            "and ((?7 between flight_departure_date_start and flight_departure_date_end)" +
+            "or (?8 between flight_departure_date_start and flight_departure_date_end));",
+            nativeQuery = true)
+    List<Promo> search(String namePromo, String airline,
+                       String departurePlace, String arrivalPlace,
+                       LocalDateTime promoDateStart, LocalDateTime promoDateEnd,
+                       LocalDateTime flightDepartureDateStart, LocalDateTime flightDepartureDateEnd,
+                       Pageable pageable);
+
 }
+
