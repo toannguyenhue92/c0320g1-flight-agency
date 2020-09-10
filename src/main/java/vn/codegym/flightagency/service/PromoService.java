1c05e9d22a13143f28093419dd45adb71627a5a1
hothaodn <62532665+hothaodn@users.noreply.github.com>
2020-09-10 09:53:08 +0700

    B-Thao push code to review

--- src/main/java/vn/codegym/flightagency/service/PromoService.java
+++ src/main/java/vn/codegym/flightagency/service/PromoService.java
@@ -1,5 +1,27 @@
 package vn.codegym.flightagency.service;
 
-public class PromoService {
+import org.springframework.data.domain.Pageable;
+import vn.codegym.flightagency.model.Promo;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PromoService {
+
+    public void createNewPromo(Promo promo);
+
+    public List<Promo> getPromoRunningNow(Pageable pageable);
+
+    public List<Promo> getPromoExpired(Pageable pageable);
+
+    public List<Promo> getPromoNotActive(Pageable pageable);
+
+    public List<Promo> searchPromo(String namePromo, String airline,
+                                   String departurePlace, String arrivalPlace,
+                                   LocalDateTime promoDateStart, LocalDateTime promoDateEnd,
+                                   LocalDateTime flightDepartureDateStart, LocalDateTime flightDepartureDateEnd,
+                                   Pageable pageable);
+
+    public LocalDateTime convertDate(String date);
 }
+
